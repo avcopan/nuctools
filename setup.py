bf3123ca652a02bedc569321fb3123cc6fd5038b
avcopan <avcopan@gmail.com>
2018-05-07 15:58:01 -0400

    Clean-up

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from distutils.core import setup
 
 setup(name='nuctools',
-      version='0.1dev',
       packages=['nuctools',],
-      install_requires=[
-          'scipy',
-      ],
-      )
+      install_requires=['scipy',],)
