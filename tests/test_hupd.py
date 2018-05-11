686342e5a0f302fbabc3b3ab1e3c866712f18cb8
avcopan <avcopan@gmail.com>
2018-05-11 15:11:39 -0400

    Brings code coverage for hessian update testing to 100%.

--- tests/test_hupd.py
+++ tests/test_hupd.py
@@ -18,27 +18,13 @@ def test__quasi_newton_condition(x0, dx):
     g = nuctools.surf.mullbrow.g(x)
 
     h_bfgs = nuctools.hupd.bfgs(x, g, x0, g0, h0)
+    h_psb = nuctools.hupd.psb(x, g, x0, g0, h0)
+    h_ms = nuctools.hupd.ms(x, g, x0, g0, h0)
+    h_bofill = nuctools.hupd.bofill(x, g, x0, g0, h0)
 
     dg = numpy.subtract(g, g0)
 
     assert numpy.allclose(numpy.dot(h_bfgs, dx), dg, rtol=1e-10, atol=1e-10)
-
-
-if __name__ == '__main__':
-    x0 = numpy.array([4., 4.])
-    dx = numpy.array([2.22044605e-15, 2.22044605e-15])
-    g0 = nuctools.surf.mullbrow.g(x0)
-    h0 = nuctools.surf.mullbrow.h(x0)
-
-    x = numpy.add(x0, dx)
-    g = nuctools.surf.mullbrow.g(x)
-
-    h_bfgs = nuctools.hupd.bfgs(x, g, x0, g0, h0)
-
-    dg = numpy.subtract(g, g0)
-
-    print(dx)
-    print(numpy.dot(h_bfgs, dx))
-    print(dg)
-
-    assert numpy.allclose(numpy.dot(h_bfgs, dx), dg, rtol=1e-10, atol=1e-10)
+    assert numpy.allclose(numpy.dot(h_psb, dx), dg, rtol=1e-10, atol=1e-10)
+    assert numpy.allclose(numpy.dot(h_ms, dx), dg, rtol=1e-10, atol=1e-10)
+    assert numpy.allclose(numpy.dot(h_bofill, dx), dg, rtol=1e-10, atol=1e-10)
