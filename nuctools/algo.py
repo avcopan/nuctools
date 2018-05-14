7f42b4965410ff4b539bce75f3b9f9c34abff043
avcopan <avcopan@gmail.com>
2018-05-14 16:05:36 -0400

    Adds Newton-Raphson algorithm, albeit still a bit messy.

--- nuctools/algo.py
+++ nuctools/algo.py
@@ -55,3 +55,57 @@ def steepest_descent(lin, x0, s0, smax, gtol=1e-5, maxiter=50, print_info=True):
         warnings.warn("Did not converge!")
 
     return x, info
+
+
+def newton_raphson(quad, x0, smax, gtol=1e-5, maxiter=50, print_info=True):
+    '''
+    :param quad: returns the value, gradient, and hessian as a function of the
+        point x and the previous point information (x0, g0, h0)
+    :type quad: function
+    :param x0: the starting point
+    :type x0: numpy.ndarray
+    :param smax: the maximum step size
+    :type smax: float
+    :param gtol: convergence threshold for the gradient, by maximum element
+    :type gtol: float
+    :param maxiter: the maximum number of iterations
+    :type maxiter: int
+    '''
+    x = x0
+    x0 = None
+    g0 = None
+    h0 = None
+
+    for iteration in range(1, maxiter + 1):
+        f, g, h = quad(x, x0=x0, g0=g0, h0=h0)
+
+        gmax = numpy.amax(numpy.abs(g))
+        converged = gmax < gtol
+
+        info = {'niter': iteration, 'f(x)': f, 'gmax': gmax,
+                'conv_status': converged}
+
+        if print_info:
+            print(info)
+
+        if converged:
+            break
+
+        dx = -numpy.dot(numpy.linalg.pinv(h), g)
+        dx = truncate_length(dx, smax)
+
+        x0 = x
+        g0 = g
+        h0 = h
+
+        x = x0 + dx
+
+    if not converged:
+        warnings.warn("Did not converge!")
+
+    return x, info
+
+
+def truncate_length(v, max_norm):
+    norm = numpy.linalg.norm(v)
+    return (v if norm < max_norm else max_norm * v / norm)
--- tests/test_algo.py
+++ tests/test_algo.py
@@ -10,6 +10,33 @@ def lin(x):
     return f, g
 
 
+def quad(x, x0=None, g0=None, h0=None):
+    f = nuctools.surf.mullbrow.f(x)
+    g = nuctools.surf.mullbrow.g(x)
+    h = nuctools.surf.mullbrow.h(x)
+    return f, g, h
+
+
+def quad_bfgs(x, x0=None, g0=None, h0=None):
+    f = nuctools.surf.mullbrow.f(x)
+    g = nuctools.surf.mullbrow.g(x)
+    if any(thing is None for thing in (x0, g0, h0)):
+        h = nuctools.surf.mullbrow.h(x)
+    else:
+        h = nuctools.hupd.bfgs(x, g, x0, g0, h0)
+    return f, g, h
+
+
+def quad_bofill(x, x0=None, g0=None, h0=None):
+    f = nuctools.surf.mullbrow.f(x)
+    g = nuctools.surf.mullbrow.g(x)
+    if any(thing is None for thing in (x0, g0, h0)):
+        h = nuctools.surf.mullbrow.h(x)
+    else:
+        h = nuctools.hupd.bofill(x, g, x0, g0, h0)
+    return f, g, h
+
+
 def test__steepest_descent():
     x, info = nuctools.algo.steepest_descent(
         lin, (-0.5, 1.4), 1e-5, 0.3, 1e-6, 50, True)
@@ -26,3 +53,81 @@ def test__steepest_descent():
 
     with pytest.warns(UserWarning):
         nuctools.algo.steepest_descent(lin, (0., 0.), 1., 1., maxiter=1)
+
+
+def test__newton_raphson():
+    print('minima:')
+
+    x0 = (-0.5, 1.4)
+    x, info = nuctools.algo.newton_raphson(
+        quad, x0, 0.3, 1e-6, 50, True)
+    print(x)
+    assert numpy.allclose(x, [-0.55822363, 1.44172584])
+    assert info['niter'] <= 6
+
+    x, info = nuctools.algo.newton_raphson(
+        quad_bfgs, x0, 0.3, 1e-6, 50, True)
+    print(x)
+    assert numpy.allclose(x, [-0.55822363, 1.44172584])
+    assert info['niter'] <= 7
+
+    x0 = (+0.6, 0.0)
+    x, info = nuctools.algo.newton_raphson(
+        quad, x0, 0.3, 1e-6, 50, True)
+    print(x)
+    assert numpy.allclose(x, [0.62349941, 0.02803776])
+    assert info['niter'] <= 5
+
+    x, info = nuctools.algo.newton_raphson(
+        quad_bfgs, x0, 0.3, 1e-6, 50, True)
+    print(x)
+    assert numpy.allclose(x, [0.62349941, 0.02803776])
+    assert info['niter'] <= 7
+
+    x0 = (-0.1, 0.5)
+    x, info = nuctools.algo.newton_raphson(
+        quad, x0, 0.3, 1e-6, 50, True)
+    print(x)
+    assert numpy.allclose(x, [-0.05001082, 0.46669411])
+    assert info['niter'] <= 5
+
+    x, info = nuctools.algo.newton_raphson(
+        quad_bfgs, x0, 0.3, 1e-6, 50, True)
+    print(x)
+    assert numpy.allclose(x, [-0.05001082, 0.46669411])
+    assert info['niter'] <= 7
+
+    print('transition states:')
+
+    x0 = (-0.8, 0.6)
+    x, info = nuctools.algo.newton_raphson(
+        quad, x0, 0.3, 1e-6, 50, True)
+    print(x)
+    assert numpy.allclose(x, [-0.82200156, 0.6243128])
+    assert info['niter'] <= 5
+
+    x, info = nuctools.algo.newton_raphson(
+        quad_bofill, x0, 0.3, 1e-6, 50, True)
+    print(x)
+    assert numpy.allclose(x, [-0.82200156, 0.6243128])
+    assert info['niter'] <= 8
+
+    x0 = (+0.2, 0.3)
+    x, info = nuctools.algo.newton_raphson(
+        quad, x0, 0.3, 1e-6, 50, True)
+    print(x)
+    assert numpy.allclose(x, [0.21248658, 0.29298833])
+    assert info['niter'] <= 5
+
+    x, info = nuctools.algo.newton_raphson(
+        quad_bofill, x0, 0.3, 1e-6, 50, True)
+    print(x)
+    assert numpy.allclose(x, [0.21248658, 0.29298833])
+    assert info['niter'] <= 6
+
+    with pytest.warns(UserWarning):
+        nuctools.algo.newton_raphson(quad_bfgs, (0., 0.), 1., 1., maxiter=1)
+
+
+if __name__ == '__main__':
+    test__newton_raphson()
