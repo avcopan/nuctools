22f59d66fac933ed68e8347afb4fb53635098d06
avcopan <avcopan@gmail.com>
2018-05-15 17:48:32 -0400

    Corrects Cerjan-Miller script.

--- scripts/cm_fig6.py
+++ scripts/cm_fig6.py
@@ -1,6 +1,8 @@
 import numpy
 from nuctools.opt import saddle
-from nuctools.surfaces.cerjanmiller import f, g, h
+from nuctools.surf.cerjmill import f, g, h
+
+import matplotlib.pyplot as pyplot
 
 p = (0.1, 0.2)
 
@@ -9,11 +11,8 @@ print(g((1, 0)))
 print(h((1, 0)))
 
 x, traj = saddle.rational_function_optimization(
-        f=f, x0=p, g=g, h=h, smax=1., order=1)
+    f=f, x0=p, g=g, h=h, smax=1., order=1)
 print(x)
-
-
-import matplotlib.pyplot as pyplot
 
 X = numpy.linspace(-0.1, 2.1)
 Y = numpy.linspace(-1, 1)
